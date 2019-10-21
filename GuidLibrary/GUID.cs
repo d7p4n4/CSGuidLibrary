284a50c136c16db609bdf6fe755f59481d24ec62
d7p4n4 <bercikezs199999999@gmail.com>
2019-10-21 20:39:40 +0100

    Add project files.

---
+++ GuidLibrary/GUID.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuidLibrary
+{
+    [AttributeUsage(AttributeTargets.All)]
+    public class GUID : Attribute
+    {
+        private string guid { get; set; }
+
+        public GUID()
+        {
+            this.guid = Guid.NewGuid().ToString();
+        }
+
+        public GUID(string value)
+        {
+            this.guid = value;
+        }
+
+        public string getGuid()
+        {
+            return guid;
+        }
+    }
+}
